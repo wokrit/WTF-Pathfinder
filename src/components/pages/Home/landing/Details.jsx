bf6975ea04ab2f1f6a11a7f3bdbc14bfc87c76e9
aynee76 <abdulmuminzainab2@gmail.com>
2024-04-18 23:14:11 +0100

    home page

---
+++ src/components/pages/Home/landing/Details.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import details from './Details.module.css'
+import Button from '../../../layout/Button'
+import { useNavigate } from "react-router-dom"
+
+export default function Details() {
+    const navigate = useNavigate()
+
+    const handleSignup = () => {
+        navigate("/Signup");
+    }
+
+  return (
+    <div className={details.container}>
+        <section className={details.article}>
+            <img src="..\src\assets\blackgirl.png" className={details.girl}/>
+            <div className={details.contents}>
+                <h3 className={details.heading}>
+                    Personalised Career Recommendation
+                </h3>
+                <p className={details.desc}>Women Techsters Pathfinder psychometric assessments delve deep into your psychological attributes, cognitive abilities, personality traits, aptitudes, and skills. Whether you're inclined towards software development, data science, cybersecurity, or UX design, our platform helps you discover the perfect fit.</p>
+            </div>
+        </section>
+
+        <section className={details.article}>
+            <div className={details.content}>
+                <h3 className={details.heading}>
+                    Clarity and Direction
+                </h3>
+                <p className={details.desc}>Women Techsters Pathfinder offers clarity and direction by highlighting areas where you excel. Whether you're a recent graduate exploring entry-level positions or a seasoned professional considering a career switch, our platform equips you with the insights needed to make confident decisions about your tech career trajectory.</p>
+            </div>
+
+            <img src="..\src\assets\pinkgirl.png" className={details.girl} />
+        </section>
+
+        <section className={details.ctatwo}>
+            <p>Get a tailored career recommendations that align with your strengths and aspirations in the Tech Industry.</p>
+
+            <Button type="button" variant="primary" onClick={handleSignup}>
+            Take Assesment
+          </Button>
+        </section>
+    </div>
+  )
+}
---
+++ src/components/pages/Home/landing/Hero.jsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import Button from '../../../layout/Button'
+import { useNavigate } from "react-router-dom"
+import hero from './Hero.module.css'
+
+
+
+export default function Hero() {
+  const navigate = useNavigate()
+
+    const handleSignup = () => {
+        navigate("/Signup");
+    }
+
+  return (
+    <div className={hero.container}>
+        <div className={hero.hero}>
+        <div className={hero.cta}>
+          <h1>Unlock Your Future with <span className={hero.purple}>Path-Finder</span>!</h1>
+
+          <p className={hero.desc}>Find Your Perfect Tech Career Match Through Psychometric Evaluation</p>
+
+          <Button type="button" variant="primary" onClick={handleSignup}>
+            Take Assesment
+          </Button>
+        </div>
+        <img src="..\src\assets\Hero Slide Accordion.png"></img>
+      </div>
+      
+      <div className={hero.metric}>
+        <div>
+          <h3>18.0%</h3>
+          <p>User Satisfaction Rate</p>
+        </div>
+
+        <div>
+          <h3>18.0%</h3>
+          <p>User Satisfaction Rate</p>
+        </div>
+
+        <div>
+          <h3>18.0%</h3>
+          <p>User Satisfaction Rate</p>
+        </div>
+
+        <div>
+          <h3>18.0%</h3>
+          <p>User Satisfaction Rate</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
